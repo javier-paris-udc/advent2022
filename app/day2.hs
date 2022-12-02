79b4ef73da464055069c9ce9f7eae852cb7419e4
Javier París <javier.paris@udc.es>
2022-12-02 15:10:10 +0100

    day2 solution

---
+++ app/day2.hs
@@ -0,0 +1,72 @@
+module Main where
+import System.Environment (getArgs, getProgName)
+import Text.Parsec (
+     parse
+    ,newline
+    ,(<|>)
+    ,char
+    ,space
+    ,sepEndBy1)
+import Text.Parsec.String (Parser)
+
+type Play = (Int, Int)
+
+
+win :: Int -> Int -> Bool
+win x y = x == (y+1) `mod` 3
+
+
+tie :: Int -> Int -> Bool
+tie = (==)
+
+
+scoreRound :: Play -> Int
+scoreRound (x,y)
+    | win y x   = 6
+    | tie y x   = 3
+    | otherwise = 0
+
+
+getDesiredOutcome :: Play -> Play
+getDesiredOutcome (x, des)
+    | des == 0  = (x, (x + 2) `mod` 3)
+    | des == 1  = (x, x)
+    | des == 2  = (x, (x + 1) `mod` 3)
+
+
+solveP2 :: [Play] -> Int
+solveP2 p = solveP1 plays
+  where
+    plays = getDesiredOutcome <$> p
+
+
+solveP1 :: [Play] -> Int
+solveP1 p = sum (scoreRound <$> p) + sum (((+1).snd) <$> p)
+
+
+gameP :: Parser [Play]
+gameP = playP `sepEndBy1` newline
+  where
+    playP     = do {p1 <- rpsP; space; p2 <- rpsP; return (p1, p2) }
+    rpsP      = rockP <|> paperP <|> scissorsP
+    rockP     = (char 'A' <|> char 'X') >> return 0
+    paperP    = (char 'B' <|> char 'Y') >> return 1
+    scissorsP = (char 'C' <|> char 'Z') >> return 2
+
+
+main :: IO ()
+main =
+    do
+        args <- getArgs
+        prog <- getProgName
+        case args of
+            [inputFile] ->
+                do
+                    input <- readFile inputFile
+                    case parse gameP inputFile input of
+                        Left err -> print err
+                        Right plays ->
+                            do
+                                print $ solveP1 plays
+                                print $ solveP2 plays
+            _ -> putStrLn $ "Use: "++prog++" input"
