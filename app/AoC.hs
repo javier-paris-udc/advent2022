309513bdbe3dfb7dba5bbfd9147dc2863092fb25
Javier París <javier.paris@udc.es>
2022-12-10 13:18:06 +0100

    day10 solution

--- app/AoC.hs
+++ app/AoC.hs
@@ -12,6 +12,7 @@ import Text.Parsec.Pos    (SourceName)
 import Text.Parsec.String (Parser)
 import System.Environment (getArgs, getProgName)
 import Control.Monad      (void)
+import Data.Bifunctor (first)
 
 
 intP :: Parsec String a Int
@@ -30,24 +31,42 @@ blanksP :: Parsec String a ()
 blanksP = void $ many1 blankP
 
 
+parseFromArg :: Parsec String s a -> s -> IO (Either String a)
+parseFromArg parser state = do
+    args <- getArgs
+    prog <- getProgName
+    case args of
+        [inputFile] ->
+            do
+                fileContent <- readFile inputFile
+                return $ first show $ runParser parser state inputFile fileContent
+        _ ->
+            return $ Left $ "Use: "++prog++" input"
+
+
+applyInputSWith :: Parsec String s a
+                -> s
+                -> (a -> b)
+                -> (a -> c)
+                -> (b -> IO ())
+                -> (c -> IO ())
+                -> IO ()
+applyInputSWith parser state solveP1 solveP2 printP1 printP2 =
+    do
+        parseRes <- parseFromArg parser state
+        case parseRes of
+            Left err ->
+                print err
+            Right parsedRes ->
+                do
+                    printP1 $ solveP1 parsedRes
+                    printP2 $ solveP2 parsedRes
+
+
 applyInput :: (Show b, Show c) => Parser a -> (a -> b) -> (a -> c) -> IO ()
 applyInput = flip applyInputS ()
 
 
 applyInputS :: (Show b, Show c) => Parsec String s a -> s -> (a -> b) -> (a -> c) -> IO ()
 applyInputS parser state solveP1 solveP2 =
-    do
-        args <- getArgs
-        prog <- getProgName
-        case args of
-            [inputFile] ->
-                do
-                    fileContent <- readFile inputFile
-                    case runParser parser state inputFile fileContent of
-                        Left err ->
-                            print err
-                        Right parsedRes ->
-                            do
-                                print $ solveP1 parsedRes
-                                print $ solveP2 parsedRes
-            _ -> print $ "Use: "++prog++" input"
+    applyInputSWith parser state solveP1 solveP2 print print
---
+++ app/day10.hs
@@ -0,0 +1,68 @@
+module Main where
+
+import AoC            (applyInputSWith, blanksP, intP)
+import Text.Parsec    (Parsec
+                      ,(<|>)
+                      ,getState
+                      ,modifyState
+                      ,newline
+                      ,sepEndBy
+                      ,string)
+import Data.Bifunctor (bimap, first)
+import Data.List      (intercalate, intersperse, unfoldr)
+import Data.Vector    (Vector, (!), fromList)
+
+
+type Cycle     = Int
+type CPUState  = (Cycle, Int)
+type RegValues = Vector Int
+
+
+groupIn :: Int -> [a] -> [[a]]
+groupIn n = unfoldr (\l -> if null l then Nothing else Just $ splitAt n l)
+
+
+solveP2 :: RegValues -> String
+solveP2 v = (++ "\n") $ intercalate "\n" $ groupIn 40 $ map pixelAt [0..239]
+  where
+    pixelAt i
+        | distance (v ! i) (i `rem` 40) < 2  = '#'
+        | otherwise                          = ' '
+    distance x y = abs (x - y)
+
+
+solveP1 :: RegValues -> Int
+solveP1 v = sum $ map (\i -> i * v `at` i) [20, 60, 100, 140, 180, 220]
+  where
+    at v i = v ! (i - 1)
+
+
+noopP :: Parsec String CPUState [Int]
+noopP = do
+    string "noop"
+    v <- snd <$> getState
+    modifyState (first (+1))
+    return [v]
+
+
+addxP :: Parsec String CPUState [Int]
+addxP = do
+    string "addx"
+    blanksP
+    inc <- intP
+    v1  <- snd <$> getState
+    modifyState (bimap (+2) (+inc))
+    v2  <- snd <$> getState
+    return [v1, v2]
+
+
+instP :: Parsec String CPUState [Int]
+instP = noopP <|> addxP
+
+
+regChangesP :: Parsec String CPUState RegValues
+regChangesP = fromList . (1:) . concat <$> instP `sepEndBy` newline
+
+
+main :: IO ()
+main = applyInputSWith regChangesP (1, 1) solveP1 solveP2 print putStrLn
