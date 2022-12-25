f045fb2c9c5925cdcd039502e7bd528ec5e23f4a
Javier París <javier.paris@udc.es>
2022-12-25 21:09:59 +0100

    removed warnings

--- app/day20.hs
+++ app/day20.hs
@@ -2,10 +2,9 @@ module Main where
 
 
 import           AoC                (applyInput, intP)
-import           Data.Foldable      (toList)
 import qualified Data.IntMap        as Map
 import           Data.IntMap        ((!), IntMap)
-import           Data.List          (foldl', foldl1', zip)
+import           Data.List          (foldl', foldl1')
 import           Data.Maybe         (fromJust)
 import qualified Data.Sequence      as Seq
 import           Data.Sequence      (Seq)
@@ -14,25 +13,25 @@ import           Text.Parsec.String (Parser)
 
 
 mixOne :: IntMap Int -> Seq Int -> Int -> Seq Int
-mixOne m seq i
-    | from == to = seq
-    | otherwise  = Seq.insertAt to i $ Seq.deleteAt from seq
+mixOne m seqToMix i
+    | from == to = seqToMix
+    | otherwise  = Seq.insertAt to i $ Seq.deleteAt from seqToMix
   where
-    from = fromJust $ Seq.findIndexL (==i) seq
+    from = fromJust $ Seq.findIndexL (==i) seqToMix
     to   = (from + m ! i) `mod` (Map.size m - 1)
 
 mixAll :: IntMap Int -> Seq Int -> Seq Int
-mixAll m seq = foldl' (mixOne m) seq [0 .. Map.size m - 1]
+mixAll m seqToMix = foldl' (mixOne m) seqToMix [0 .. Map.size m - 1]
 
 
 solve :: Int -> [Int] -> Int
 solve n l = sum $ map (Seq.index resSeq) positions
   where
     m         = Map.fromAscList $ zip [0 .. ] l
-    seq       = Seq.fromList [0 .. Map.size m - 1]
+    seqToMix  = Seq.fromList [0 .. Map.size m - 1]
 
     mix       = foldl1' (.) $ replicate n (mixAll m)
-    resSeq    = (m !) <$> mix seq
+    resSeq    = (m !) <$> mix seqToMix
 
     pos0      = fromJust $ Seq.findIndexL (==0) resSeq
     positions = (`mod` Map.size m) <$> [pos0 + 1000, pos0 + 2000, pos0 + 3000]
